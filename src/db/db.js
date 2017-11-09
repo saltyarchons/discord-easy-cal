579623cb5f12c9622a458de7aa445c3e935dd5c5
Francis <francis.screene@gmail.com>
2017-11-09 21:39:24 +0000

    Added basic db functions

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,7 @@
 const elasticsearch = require('elasticsearch');
 
-const CALENDAR_INDEX = 'calendara';
+const CALENDAR_INDEX = 'calendar';
+const CALENDAR_TYPE = CALENDAR_INDEX;
 
 let client = new elasticsearch.Client({
     host: 'localhost:9200', //TODO(tree): make this configurable
@@ -24,3 +25,35 @@ client.indices.exists({index: CALENDAR_INDEX}, (error, exists) => {
         console.log('Successfully created calendar index');
     })
 });
+
+module.exports.getAllCalendars = () => {
+    return client.search({
+        index: CALENDAR_INDEX,
+        body: {
+            query: {
+                match_all: {}
+            }
+        }
+    }).then(result => {
+        return result.hits.hits;
+    })
+};
+
+module.exports.putCalendar = (calendar) => {
+    if (!('id' in calendar)) {
+        throw 'Calendar ' + calendar + ' doesn\'t contain id column';
+    }
+    client.index({
+        index: CALENDAR_INDEX,
+        type: CALENDAR_TYPE,
+        id: calendar.id,
+        body: calendar
+    }, (error, response) => {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log(`Calendar with ${calendar.id} has successfully been inserted`);
+        }
+
+    })
+};
