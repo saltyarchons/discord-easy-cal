0d3704a0da9eb184af1ca81bc56c14cbf03be17b
Lex Webb <lexwebb@gmail.com>
2017-11-11 14:56:29 +0000

    Added input validation and response confirmation

--- src/events/create.js
+++ src/events/create.js
@@ -6,8 +6,16 @@ exports.run = (client, logger, message) => {
 
     const waitTime = moment.duration(100, 'seconds').milliseconds();
     conversationHandler
-        .ask('Please enter a name for the event', waitTime)
-        .then('Please enter a start time for the event', waitTime)
+        .ask('Please enter a name for the event', waitTime, undefined, (content) => {
+            return `Please confirm that the event name "${content}" is corrrect.`;
+        })
+        .then('Please enter a start time for the event', waitTime, (content) => {
+            if (content === 'banana') {
+                return { passed: true };
+            }
+
+            return { passed: false, message: 'The response should be banana' };
+        })
         .then('Please enter an end time for the event', waitTime)
         .then('Please enter the location for the event', waitTime)
         .end('Thanks!')
--- src/messaging/conversationHandler.js
+++ src/messaging/conversationHandler.js
@@ -7,14 +7,17 @@ exports.ConversationHandler = class {
         this.replies = [];
     }
 
-    ask(question, timeout) {
+    ask(question, timeout, validationCallback, confirmCallback) {
         this.messageChain = [];
-        this.messageChain.push({ type: 'question', question, timeout });
-        return this;
+        this.replies = [];
+
+        return this.then(question, timeout, validationCallback, confirmCallback);
     }
 
-    then(question, timeout) {
-        this.messageChain.push({ type: 'question', question, timeout });
+    then(question, timeout, validationCallback, confirmCallback) {
+        this.messageChain.push({
+            type: 'question', question, timeout, validationCallback, confirmCallback,
+        });
         return this;
     }
 
@@ -45,6 +48,43 @@ exports.ConversationHandler = class {
         }, { time: question.timeout });
 
         collector.on('collect', (response) => {
+            if (question.validationCallback) {
+                const result = question.validationCallback(response.content);
+                if (result.passed !== true) {
+                    message.channel.send(result.message);
+                    this.messageChain.unshift(question);
+                    this.run(response, callback);
+                    collector.cleanup();
+                    return;
+                }
+            }
+
+            if (question.confirmCallback) {
+                let confirmText = question.confirmCallback(response.content);
+                confirmText += ' (Yes|No)';
+
+                const confirmCollector = new Discord.MessageCollector(message.channel, (m) => {
+                    return m.author.id === message.author.id;
+                }, { time: question.timeout });
+
+                message.channel.send(confirmText);
+                collector.cleanup();
+
+                confirmCollector.on('collect', (confirmResponse) => {
+                    if (confirmResponse.content.toLowerCase() === 'yes'
+                        || confirmResponse.content.toLowerCase() === 'y') {
+                        this.run(response, callback);
+                    } else {
+                        this.messageChain.unshift(question);
+                        this.run(response, callback);
+                    }
+
+                    confirmCollector.cleanup();
+                });
+
+                return;
+            }
+
             this.replies.push({ question, response: response.content });
             this.run(response, callback);
             collector.cleanup();
