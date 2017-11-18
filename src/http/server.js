3f659ffd61467c8d5e771aa965339100d970b3d8
sdf010 <safwaan1@live.co.uk>
2017-11-18 16:45:40 +0000

    adding port to config file
    the http server now has the option to set port and has to be started by calling  the start() function. The app now get the port option fromt he config file and passes it to the http server

--- src/http/server.js
+++ src/http/server.js
@@ -2,14 +2,26 @@ const http = require('http');
 const url = require('url');
 const logger = require('winston');
 
-http.createServer((req, res) => {
-    const urlparameters = url.parse(req.url, true);
-    // TODO: make path read from config
-    if (urlparameters.pathname === '/easyCalAuth') {
-        const token = urlparameters.query.code;
-        const guild = urlparameters.query.state;
-        logger.info(token);
-        logger.info(guild);
+let lport = 8080;
+
+module.exports.setPort = (port) => {
+    if (typeof port === 'number' && port <= 65535) {
+        lport = port;
+    } else {
+        throw new RangeError('Invlaid Listening Port');
     }
-    res.write('Hello World!');
-}).listen(8080);
+};
+
+module.exports.start = () => {
+    http.createServer((req, res) => {
+        const urlparameters = url.parse(req.url, true);
+        // TODO: make path read from config
+        if (urlparameters.pathname === '/easyCalAuth') {
+            const token = urlparameters.query.code;
+            const guild = urlparameters.query.state;
+            logger.info(token);
+            logger.info(guild);
+        }
+        res.write('Hello World!');
+    }).listen(lport);
+};
